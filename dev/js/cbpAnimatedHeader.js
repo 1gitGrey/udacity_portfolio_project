24c02531ec2bdbd8b382175bb5c6e5535e81adff
Grey Grissom <grey.grissom@gmail.com>
2016-12-29 00:35:35 -0800

    staged \ colors

--- dev/js/cbpAnimatedHeader.js
+++ dev/js/cbpAnimatedHeader.js
@@ -2,9 +2,9 @@
 var cbpAnimatedHeader = (function() {
 
 	var docElem = document.documentElement,
-		header = document.querySelector( '.cbp-af-header' ),
+		header = document.querySelector( '.navbar-fixed-top' ),
 		didScroll = false,
-		changeHeaderOn = 300;
+		changeHeaderOn = 200;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
@@ -18,10 +18,10 @@ var cbpAnimatedHeader = (function() {
 	function scrollPage() {
 		var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'cbp-af-header-shrink' );
+			classie.remove( header, 'navbar-expanded' );
 		}
 		else {
-			classie.remove( header, 'cbp-af-header-shrink' );
+			classie.add( header, 'navbar-expanded' );
 		}
 		didScroll = false;
 	}
